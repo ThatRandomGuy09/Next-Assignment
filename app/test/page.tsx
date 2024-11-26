4608dc976d4fd353e28e8ceb0a906dbfe2e934d3
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-26 19:18:32 +0530

    Created the Tes Package Page

---
+++ app/test/page.tsx
@@ -0,0 +1,17 @@
+import TestNavigation from "@/app/components/TestNavigation";
+import Sidebar from "../components/Sidebar";
+import ExamList from "../components/ExamList";
+
+export default function TestPage() {
+  return (
+    <div className="flex flex-col lg:flex-row">
+      <div className="fixed lg:relative h-full w-64 text-white">
+        <Sidebar />
+      </div>
+      <div className="flex-1 ml-0 lg:ml-64 mt-6 mx-4 lg:mx-8">
+        <TestNavigation />
+        <ExamList />
+      </div>
+    </div>
+  );
+}
