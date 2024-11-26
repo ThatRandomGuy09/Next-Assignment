968c3971343da4d030100d92c214886e296fb81c
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-26 20:47:40 +0530

    Deployment bug fixes

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -24,7 +24,7 @@ const Sidebar: FC = () => {
   ];
 
   const handleCourseClick = () => {
-    router.push("/course");
+    router.push("/my-course");
     setIsMobileMenuOpen(false);
   };
 
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,7 +5,7 @@ import ExamList from "../components/ExamList";
 export default function TestPage() {
   return (
     <div className="flex flex-col lg:flex-row">
-      <div className="fixed lg:relative h-full w-64 text-white">
+      <div className="fixed h-full w-64 text-white">
         <Sidebar />
       </div>
       <div className="flex-1 ml-0 lg:ml-64 mt-6 mx-4 lg:mx-8">
